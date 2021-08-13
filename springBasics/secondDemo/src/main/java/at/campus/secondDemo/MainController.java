6b43e2e7c82d1fd3a199bb5361631821331f0e53
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-08-13 23:21:00 +0200

    update secondDemo

--- springBasics/secondDemo/src/main/java/at/campus/secondDemo/MainController.java
+++ springBasics/secondDemo/src/main/java/at/campus/secondDemo/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(path = "/demo")
 public class MainController {
@@ -22,11 +24,16 @@ public class MainController {
         return "saved.";
     }
 
+    @GetMapping(path = "/findAll")
+    private @ResponseBody
+    List<Person> findAll() {
+        return personRepository.findAll();
+    }
+
     @GetMapping(path = "/all")
     private @ResponseBody
     Iterable<Person> getAllPeople() {
         return personRepository.findAll();
     }
-
 
 }
