17a6a4be63a4825832acb5d138fea7b322715231
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-08-18 12:36:18 +0200

    init StudentRepository interface

--- springBasics/thirdDemo/springDataJPA/src/test/java/at/campus/springDataJPA/repository/StudentRepositoryTest.java
+++ springBasics/thirdDemo/springDataJPA/src/test/java/at/campus/springDataJPA/repository/StudentRepositoryTest.java
@@ -1,4 +1,46 @@
+package at.campus.springDataJPA.repository;
+
+import at.campus.springDataJPA.entity.Guardian;
+import at.campus.springDataJPA.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class StudentRepositoryTest {
-  
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudent() {
+        Student student = Student.builder()
+                .firstName("Alexander")
+                .lastName("Polzhofer")
+                .emailId("alexander@polzhofer.com")
+//                .guardianName("knight")
+//                .guardianEmail("knight@mail.com")
+//                .guardianMobile("436015278925465")
+                .build();
+        this.studentRepository.save(student);
+    }
+
+    @Test
+    public void saveStudentWithGuardian() {
+        Guardian guardian = Guardian.builder()
+                .name("guardianTheEvil")
+                .mobileNumber("0043658792451")
+                .email("guardian@test.com")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("Person")
+                .lastName("Test")
+                .emailId("person@test.com")
+                .guardian(guardian)
+                .build();
+        this.studentRepository.save(student);
+    }
 }
